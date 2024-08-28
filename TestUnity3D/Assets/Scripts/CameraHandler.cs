25525d6e56eb4a92505de13e97ba66052b9ab6b2
HappyMaxT3 <m9775531181@gmail.com>
2024-08-28 21:59:00 +0300

    recreated repository (one fucking script was duplicate)

---
+++ TestUnity3D/Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Main
+{
+    public class CameraHandler : MonoBehaviour
+    {
+        public Transform targetTransform;
+        public Transform cameraTransform;
+        public Transform cameraPivotTransform;
+
+        private Transform myTransform;
+        private Vector3 cameraTransformPos;
+        private LayerMask ignoreLayers;
+        private Vector3 cameraFollowVelocity = Vector3.zero;
+
+        public static CameraHandler singleton;
+
+        [SerializeField] public float lookSpeed = 0.1f;
+        [SerializeField] public float  followSpeed = 0.1f;
+        [SerializeField] public float pivotSpeed = 0.03f;
+
+        private float defaultPosition;
+        private float targetPosition;
+        private float lookAngle;
+        private float pivotAngle;
+
+        [SerializeField] public float minPivot = -35;
+        [SerializeField] public float maxPivot = 35;
+        [SerializeField] public float cameraSphereRadius = 0.1f;
+        [SerializeField] public float cameraCollisionOffset = 0.1f;
+        [SerializeField] public float minCollisionOffset = 0.1f;
+
+        private void Awake()
+        {
+            singleton = this;
+            myTransform = transform;
+            defaultPosition = cameraTransform.localPosition.z;
+            ignoreLayers = ~(1 << 8 | 1 << 9 | 1 << 10);
+        }
+
+        public void FollowTarget(float delta)
+        {
+            Vector3 targetPosition = Vector3.SmoothDamp(myTransform.position, targetTransform.position, ref cameraFollowVelocity, delta / followSpeed);
+            myTransform.position = targetPosition;
+            HandleCameraCollisions(delta);
+        }
+
+
+        public void HandleCameraLocation(float delta, float mouseXInput, float mouseYInput)
+        {
+            lookAngle += (mouseXInput * lookSpeed) / delta;
+            pivotAngle -= (mouseYInput * pivotSpeed) / delta;
+            pivotAngle = Mathf.Clamp(pivotAngle, minPivot, maxPivot);
+
+            Vector3 rotation = Vector3.zero;
+            rotation.y = lookAngle;
+            Quaternion targetRotation = Quaternion.Euler(rotation);
+            myTransform.rotation = targetRotation;
+
+            rotation = Vector3.zero;
+            rotation.x = pivotAngle;
+
+            targetRotation = Quaternion.Euler(rotation);
+            cameraPivotTransform.localRotation = targetRotation;
+        }
+
+        private void HandleCameraCollisions(float delta)
+        {
+            targetPosition = defaultPosition;
+            RaycastHit hit;
+            Vector3 direction = cameraTransform.position - cameraPivotTransform.position;
+            direction.Normalize();
+
+            if (Physics.SphereCast(cameraPivotTransform.position, cameraSphereRadius, direction, out hit, Mathf.Abs(targetPosition), ignoreLayers) == true)
+            {
+                float distance = Vector3.Distance(cameraPivotTransform.position, hit.point);
+                targetPosition = -(distance - cameraCollisionOffset);
+            }
+            if(Mathf.Abs(targetPosition) < minCollisionOffset)
+            {
+                targetPosition = -minCollisionOffset;
+            }
+            cameraTransformPos.z = Mathf.Lerp(cameraTransform.localPosition.z, targetPosition, delta / 0.1f);
+            cameraTransform.localPosition = cameraTransformPos;
+        }
+    }
+}
+
+
